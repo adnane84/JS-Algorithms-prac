5594fe6f8b6b5a37aec6ab442782745fea90c247
adnane84 <aadn2110@gmail.com>
2023-02-19 23:46:07 -0500

    first commit

--- quickSortPrac.js
+++ quickSortPrac.js
@@ -41,7 +41,7 @@ console.log(quickSort([9, 16, 0, 0, 7, 3, 0, -2, 5, 15, 8, 7, 8, 2]));
 // at which point they are combined to form the final sorted array.
 
 // The implementation of this function has a time complexity of O(n log n) in the average and best cases because
-// the partitioning of the array is relatively balanced. In each recursive call, the array is partitioned into two subarrays 
+// the partitioning of the array is relatively balanced. In each recursive call, the array is partitioned into two subarrays
 // that are roughly half the size of the original array. This leads to log n levels of recursion,
 // and each level requires O(n) time for the partitioning operation, resulting in a total time complexity of O(n log n).
 
@@ -53,3 +53,33 @@ console.log(quickSort([9, 16, 0, 0, 7, 3, 0, -2, 5, 15, 8, 7, 8, 2]));
 
 // In summary, the quickSort function you provided has a time complexity of O(n log n) in the average and best cases,
 // and O(n^2) in the worst case.
+
+
+// const array1 = [3, 7, 1, 4, 9, 2, 6, 5, 8]; // [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// const array2 = [5, 3, 1, 4, 2]; [ 1, 2, 3, 4, 5 ]
+
+
+function quickSort(array) {
+  if (array.length <= 1) {
+    return array;
+  }
+  let pivotIndex = Math.floor(Math.random(array.length));
+  let pivot = array[pivotIndex];
+  let left = [];
+  let right = [];
+  for (let i = 0; i < array.length; i++) {
+    if (i === pivotIndex) {
+      continue;
+    }
+    if (array[i] < pivot) {
+      left.push(array[i]);
+    } else {
+      right.push(array[i]);
+    }
+
+  }
+  const leftSort = quickSort(left);
+  const rightSort = quickSort(right);
+
+  return [...leftSort, pivot, ...rightSort];
+}
